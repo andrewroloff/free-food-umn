1fb149816b00cb4343517231e87d52f4f12b9b01
Andrew Roloff <drewrol@icloud.com>
2026-03-20 12:24:22 -0500

    I don't like cloudflare anymore

--- functions/cron.js
+++
@@ -1,9 +0,0 @@
-export async function onScheduled(event, env) {
-    const now = Math.floor(Date.now() / 1000);
-
-    await env.DB.prepare(
-        "DELETE FROM posts WHERE expires_at <= ?"
-    )
-        .bind(now)
-        .run();
-}
